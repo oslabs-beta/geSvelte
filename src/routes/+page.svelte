595f15438331302f82d08bfe39026a9f76b27f7e
Christian Ashley <ashleycz1993@gmail.com>
2023-01-30 20:47:00 -0500

    Initial setup
    Co-authored-by: sjordan2010 <sjordan2010@users.noreply.github.com>
    Co-authored-by: ksslv <ksslv@users.noreply.github.com>
    Co-authored-by: jh1551 <jh1551@users.noreply.github.com>
    Co-authored-by: Umair Shafiq <soulofstorms@hotmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<header>
+	<h3><a href="/about">About</a></h3>
+	<h3>Create a form</h3>
+</header>
+
+<style lang="scss">
+ 
+</style>
