7c7ef6ec9c94dbd5c6d6ecb37646eeccbfab48c3
Christian Ashley <ashleycz1993@gmail.com>
2023-02-02 17:52:15 -0500

    Begin building out page routes Created Header UI component Created assets static directory in lib
    Co-authored-by: Stefan Jordan <sjordan2010@gmail.com>
    Co-authored-by: Johnny Howell <tsjohnnyh@gmail.com>
    Co-authored-by: Ksenia <45905849+ksslv@users.noreply.github.com>
    Co-authored-by: Umair Shafiq <soulofstorms@hotmail.com>

---
+++ src/lib/ui/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+import logo from '$lib/assets/gesvelte-logo.png';
+</script>
+
+<header class="main-header flex justify-between">
+    <a class="site-logo" href="/"><img src={logo}  alt="geSvelte Logo"></a>
+    <nav class="main-nav flex justify-start direction-column">
+        <ul>
+            <li><a href="/docs">Docs</a></li>
+            <li><a href="/create-form">Create Form</a></li>
+            <li><a href="/stage">Stage</a></li>
+        </ul>
+    </nav>
+  <h1>This is our site's header</h1>
+</header>
+
+
+<style>
+
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Header from "$lib/ui/Header.svelte";
+  import '../lib/assets/styles.css'
+</script>
+
+<Header />
+<main>
+  <slot />
+</main>
