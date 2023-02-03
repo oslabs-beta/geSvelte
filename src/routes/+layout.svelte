7040282f06503af832cd790a4cfd53e8f3997713
John Howell <tsjohnnyh@gmail.com>
2023-02-02 19:24:16 -0600

    some css stuff, rangeSlider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script>
-  import Header from "$lib/ui/Header.svelte";
-  import '../lib/assets/styles.css'
+	import Header from '$lib/ui/Header.svelte';
+	import RangeSlider from '$lib/components/base/RangeSlider.svelte';
+	import '../lib/assets/styles.css';
 </script>
 
-<Header />
 <main>
-  <slot />
+	<Header />
+	<div class="ges-row flex">
+		<div class="col-2">
+			<RangeSlider name="name" value={50} max={100} min={1} step={1}>LABEL</RangeSlider>
+		</div>
+		<div class="col-2">
+			<RangeSlider name="name" value={50} max={100} min={1} step={1}>LABEL</RangeSlider>
+		</div>
+	</div>
+	<slot />
 </main>
