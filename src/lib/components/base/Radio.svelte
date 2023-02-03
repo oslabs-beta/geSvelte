30adbc6f1d02eda19e2541026bf3122e95747432
Christian Ashley <ashleycz1993@gmail.com>
2023-02-02 20:09:47 -0500

    Add Radio base component
    Co-authored-by: Stefan Jordan <sjordan2010@gmail.com>
    Co-authored-by: Johnny Howell <tsjohnnyh@gmail.com>
    Co-authored-by: Ksenia <45905849+ksslv@users.noreply.github.com>
    Co-authored-by: Umair Shafiq <soulofstorms@hotmail.com>

---
+++ src/lib/components/base/Radio.svelte
@@ -0,0 +1,12 @@
+<script>
+  // fields will be an array of options
+  export let fields = ['test'];
+  export let name = 'default';
+</script>
+
+<fieldset>
+  {#each fields as field}
+    <input type="radio" id={field} name={name} value={field}>
+    <label for={field}>{field}</label>
+  {/each}
+</fieldset>
--- src/routes/stage/+page.svelte
+++ src/routes/stage/+page.svelte
@@ -1,6 +1,8 @@
 <script>
-  import RegistrationWrapper from "$lib/components/wrapper/RegistrationWrapper.svelte";
+  // import RegistrationWrapper from "$lib/components/wrapper/RegistrationWrapper.svelte";
+  import Radio from "$lib/components/base/Radio.svelte";
 </script>
 
 
-<RegistrationWrapper styleClass="gesvelte-styles" />
+<!-- <RegistrationWrapper styleClass="gesvelte-styles" /> -->
+<Radio fields={['test1', 'test2']} name="sample" />
