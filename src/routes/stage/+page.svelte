d69be0dbed518af878a7729380c113c5833812f2
Christian Ashley <ashleycz1993@gmail.com>
2023-02-04 20:40:24 -0500

    Initial cleanup
    Co-authored-by: Stefan Jordan <sjordan2010@gmail.com>
    Co-authored-by: Johnny Howell <tsjohnnyh@gmail.com>
    Co-authored-by: Ksenia <45905849+ksslv@users.noreply.github.com>
    Co-authored-by: Umair Shafiq <soulofstorms@hotmail.com>

--- src/routes/stage/+page.svelte
+++ src/routes/stage/+page.svelte
@@ -5,7 +5,7 @@
   import Checkbox from "$lib/components/base/Checkbox.svelte";
   import Button from "$lib/components/base/Button.svelte";
   // import { componentStore } from "../../stores/componentStores";
-  import { radioTestStore, checkboxTestStore } from "$lib/stores/componentStores";
+  // import { radioTestStore, checkboxTestStore } from "$lib/stores/componentStores";
   import { customForm } from "$lib/stores/componentStores";
 	import { onMount, setContext } from "svelte";
   import { writable } from "svelte/store";
@@ -16,30 +16,30 @@
 
   // setContext('test', checkboxTestStore);
 
-  // should we add helper functions to a 'global' (not the right word) object?
-  const addCheckbox = (e) => {
-    const newStore = Object.assign({}, $checkboxTestStore);
-    // ts error: document.querySelector flags a 'may be null' error
-    // https://stackoverflow.com/questions/52491832/how-to-use-document-getelementbyid-method-in-typescript
-    const newCheckboxField = testSubmit;
-    newStore.fields.push({value: newCheckboxField.value, checked: false});
-    newCheckboxField.value = '';
-    $checkboxTestStore = newStore;
-  };
+  // // should we add helper functions to a 'global' (not the right word) object?
+  // const addCheckbox = (e) => {
+  //   const newStore = Object.assign({}, $checkboxTestStore);
+  //   // ts error: document.querySelector flags a 'may be null' error
+  //   // https://stackoverflow.com/questions/52491832/how-to-use-document-getelementbyid-method-in-typescript
+  //   const newCheckboxField = testSubmit;
+  //   newStore.fields.push({value: newCheckboxField.value, checked: false});
+  //   newCheckboxField.value = '';
+  //   $checkboxTestStore = newStore;
+  // };
 
-  const addRadio = (e) => {
-    const newStore = Object.assign({}, $radioTestStore);
-    // ts error: document.querySelector flags a 'may be null' error
-    // https://stackoverflow.com/questions/52491832/how-to-use-document-getelementbyid-method-in-typescript
-    const newRadio = testRadio;
-    console.log(newRadio.value)
-    newStore.fields.push({value: newRadio.value, label: newRadio.value});
-    newRadio.value = '';
-    $radioTestStore = newStore;
-  };
+  // const addRadio = (e) => {
+  //   const newStore = Object.assign({}, $radioTestStore);
+  //   // ts error: document.querySelector flags a 'may be null' error
+  //   // https://stackoverflow.com/questions/52491832/how-to-use-document-getelementbyid-method-in-typescript
+  //   const newRadio = testRadio;
+  //   console.log(newRadio.value)
+  //   newStore.fields.push({value: newRadio.value, label: newRadio.value});
+  //   newRadio.value = '';
+  //   $radioTestStore = newStore;
+  // };
   
 </script>
-<div class="ges-row">
+<!-- <div class="ges-row">
   <div class="col-2">
     <Checkbox fields={$checkboxTestStore.fields} name="test" />
 
@@ -55,7 +55,7 @@
       <button type="submit">Add Radio</button>
     </form>
   </div>
-</div>
+</div> -->
 
 <div>
   <!-- {Object.keys($customForm)} -->
