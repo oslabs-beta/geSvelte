baa8bb03674411ca8b3dd900184322d5c7f7d846
Christian Ashley <ashleycz1993@gmail.com>
2023-02-08 15:54:50 -0500

    Correction to 'for' attribute in Checkbox.svelte label
    Co-authored-by: Stefan Jordan <sjordan2010@gmail.com>
    Co-authored-by: Johnny Howell <tsjohnnyh@gmail.com>
    Co-authored-by: Ksenia <45905849+ksslv@users.noreply.github.com>
    Co-authored-by: Umair Shafiq <soulofstorms@hotmail.com>

--- src/lib/components/base/Input.svelte
+++ src/lib/components/base/Input.svelte
@@ -2,10 +2,13 @@
   Input.svelte
   Base level component representing a text inputs using HTML5 inputs
   TODO: update data to be more inline with base data shape
-  params: 
-    inputType: TODO: set up union type for all acceptable text inputs
+  params:
+    placeholder: light text prompt in field when value is empty 
     labelClass: classes used with atomic CSS selectors
-    ...
+    inputType: textInputs 
+    labelText: Label/directive/prompt for user
+    id: id for input element
+    name: reference on submitted data
 -->
 
 <script lang="ts">
